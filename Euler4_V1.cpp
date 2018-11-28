4d22d89309130d212cfd8d292c3ff63a033e7063
Vijay Kumar Prakash <vijaykumar.sb.22@gmail.com>
2018-11-28 19:29:01 +0530

    Solved Euler Problem 4

---
+++ Euler4_V1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int isPalindrome(long);
+
+int main()
+{
+	int i,j;
+	long prod;
+	for(i=999; i>100; i--)
+	{
+		for(j=999;j>100;j--)
+		{
+			prod=i*j;
+			if(isPalindrome(prod))
+				break;
+		}
+	}
+	cout<<i<<" and "<<j<<" are the largest 3 digit numbers whose product "<<prod<<" is a palindrome.";
+}
+
+int isPalindrome(long num)
+{
+	long temp=num, rev=0, digit;
+	while(temp>0)
+	{
+		digit=temp%10;
+		rev=(rev*10)+digit;
+		temp/=10;
+	}
+	if(rev==num)
+		return 1;
+	else
+		return 0;
+}
